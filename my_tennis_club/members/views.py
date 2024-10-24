25b9069752b807ed1c2b87681459af61be558dd3
Timothy Jay Sayson <71298147+tvsayson@users.noreply.github.com>
2024-10-24 16:03:43 +0000

    Back to original

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.template import loader
 
 def members(request):
-  template = loader.get_template('myfirst.html')
+  template = loader.get_template('test.html')
   return HttpResponse(template.render())
